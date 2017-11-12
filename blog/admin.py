fb9f2df3f5303ed3da9b88bf169a439fa8bac6f4
weis <578402809@qq.com>
2017-11-12 22:08:06 +0800

    add post tag

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Category, Tag
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_time', 'modified_time', 'category', '__str__')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+class TageAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TageAdmin)
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,14 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=70)
