35a483454ea0ae3b2a5a07c98e9314e12f03f608
weis <578402809@qq.com>
2018-04-20 10:06:00 +0800

    add django filter backend

--- blog/views.py
+++ blog/views.py
@@ -7,18 +7,26 @@ from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, RetrieveAPIView, ListCreateAPIView
 from rest_framework.views import APIView
 from django.db.models import Count
+import django_filters.rest_framework
 
 
 from . import models
 import json
 
 # Create your views here.
+# class PostFilter(django_filters.FilterSet):
+#     class Meta:
+#         model = models.Post
+#         fields = ('post','tag')
 
 class postListView(ListAPIView):
     serializer_class = serializers.PostSerializer
+    queryset = models.Post.objects.all()
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_fields = ('category', 'tags')
     
-    def get_queryset(self):
-        return models.Post.objects.all() 
+    # def get_queryset(self):
+    #     return queryset
 
 class postRetrieveView(APIView):
     serializer_class = serializers.PostSerializer
@@ -40,7 +48,7 @@ class postRetrieveView(APIView):
 
 class tagListView(ListAPIView):
     serializer_class = serializers.TagSerializer
-    
+
     def get_queryset(self):
         return models.Tag.objects.annotate(post_count=Count('post'))
 
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'corsheaders',
     'blog'
 ]
@@ -131,4 +132,5 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     # ]
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
